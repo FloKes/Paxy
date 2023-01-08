e22798887562d7eb30f2880fa8040b1934c65bf5
Flo Kes <florian.kesten@gmail.com>
2023-01-08 13:06:34 +0100

    Implemented fault tolerance

--- acceptor.erl
+++ acceptor.erl
@@ -2,7 +2,7 @@
 -export([start/2]).
 
 
--define(delay, 1).
+-define(delay, 4000).
 -define(drop, -1).
 
   
@@ -11,13 +11,25 @@ start(Name, PanelId) ->
   spawn(fun() -> init(Name, PanelId) end).
         
 init(Name, PanelId) ->
-  Promised = order:null(), 
-  Voted = order:null(),
-  Value = na,
-  pers:open(Name),
-  pers:store(Name, Promised, Voted, Value, PanelId),
-  pers:close(Name),
-  acceptor(Name, Promised, Voted, Value, PanelId).
+  case PanelId of 
+    na ->
+      {Pro, Vot, Val, Pan} = read_state(Name),
+      io:format("--- Acceptor ~w Recovering from crash --- Promised: ~w, Voted: ~w, Value: ~w,  PID:~w~n",
+         [Name, Pro, Vot, Val, Pan]),
+
+      % Update gui
+      Colour = case Val of na -> {0,0,0}; _ -> Val end,
+      Pan ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Vot]), 
+                  "Promised: " ++ io_lib:format("~p", [Pro]), Colour},
+      acceptor(Name, Pro, Vot, Val, Pan);
+    _ ->
+      Promised = order:null(), 
+      Voted = order:null(),
+      Value = na,
+      save_state(Name, Promised, Voted, Value, PanelId),
+      acceptor(Name, Promised, Voted, Value, PanelId)
+  end.
+  
 
 acceptor(Name, Promised, Voted, Value, PanelId) ->
   receive
@@ -43,6 +55,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
           Colour = case Value of na -> {0,0,0}; _ -> Value end,
           PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
                      "Promised: " ++ io_lib:format("~p", [Round]), Colour},
+          save_state(Name, Round, Voted, Value, PanelId),
           acceptor(Name, Round, Voted, Value, PanelId);
         false ->
           %Proposer ! {sorry, {prepare, Round}},
@@ -71,6 +84,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
               % Update gui
               PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), 
                          "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
+              save_state(Name, Promised, Round, Proposal, PanelId),
               acceptor(Name, Promised, Round, Proposal, PanelId);
             false ->
               acceptor(Name, Promised, Voted, Value, PanelId)
@@ -84,3 +98,14 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
       pers:delete(Name),
       ok
   end.
+
+save_state(Name, Promised, Voted, Value, PanelId) ->
+  pers:open(Name),
+  pers:store(Name, Promised, Voted, Value, PanelId),
+  pers:close(Name).
+
+read_state(Name) ->
+  pers:open(Name),
+  {Promised, Voted, Value, PanelId} = pers:read(Name),
+  pers:close(Name),
+  {Promised, Voted, Value, PanelId}.
